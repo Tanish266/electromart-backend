b10b315e198165a653053b8676096c7365863668
Tanish Shihora <tanishshihora31@gmail.com>
2025-08-23 16:59:48 +0530

    changes a .env and cord

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://eelectromart.netlify.app",
+      "https://electromart-admin.netlify.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
